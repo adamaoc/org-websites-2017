77af207a3d5159cc547c27d42b74452b5f4d7885
Adam Moore <adamaoc@gmail.com>
2016-11-22 07:12:26 -0600

    added google site verification

--- app/views/shared/header.php
+++ app/views/shared/header.php
@@ -7,7 +7,8 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-
+    <meta name="google-site-verification" content="yQCRgjSyXaEP6ZitoJHS7sOXgVj7xJg8c0cq3aSexkY" />
+    
     <link rel="apple-touch-icon" sizes="180x180" href="http://<?= $_SERVER['HTTP_HOST'] ?>/public/images/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" href="http://<?= $_SERVER['HTTP_HOST'] ?>/public/images/favicon/favicon-32x32.png" sizes="32x32">
     <link rel="icon" type="image/png" href="http://<?= $_SERVER['HTTP_HOST'] ?>/public/images/favicon/favicon-16x16.png" sizes="16x16">
