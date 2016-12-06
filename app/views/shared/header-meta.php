96bdfecab5dd82d79f4290f5e3541eabb183df22
Adam Moore <adamaoc@gmail.com>
2016-12-05 19:09:55 -0600

    Update header-meta.php

--- app/views/shared/header-meta.php
+++ app/views/shared/header-meta.php
@@ -7,12 +7,12 @@
 <meta property="og:url" content="http://org-websites.com/" />
 <meta property="og:image" content="http://<?= $_SERVER['HTTP_HOST'] ?>/public/images/inspire-bg.jpg" />
 <meta property="og:site_name" content="ORG Websites" />
-<meta property="og:description" content="At ORG Websites we want to help you get your voice heard. If 2016 has proven anything, it's that traditional media is dying off and you can really make a difference by putting your voice or platform online. We've helped get a few grassroots campaigns and blogs started up throughout this past election cycle and we're eager to help more people. Whether you're wanting to get your word out with a simple blog or start a movement with a political website, we can help." />
+<meta property="og:description" content="We've helped many organizations begin to get the word out about their passion for helping." />
 
 <meta name="twitter:card" content="summary_large_image" />
 <meta name="twitter:site" content="@adamaoc" />
 <meta name="twitter:domain" content="org-websites.com" />
-<meta name="twitter:title" content="ORG Websites - Make a difference BE POLITICALLY ACTIVE" />
-<meta name="twitter:description" content="At ORG Websites we want to help you get your voice heard. If 2016 has proven anything, it's that traditional media is dying off and you can really make a difference by putting..." />
+<meta name="twitter:title" content="ORG-Websites - Helpping get non-profit orgs setup online" />
+<meta name="twitter:description" content="We've helped many organizations begin to get the word out about their passion for helping." />
 <meta name="twitter:image" content="http://<?= $_SERVER['HTTP_HOST'] ?>/public/images/inspire-bg.jpg" />
 <meta itemprop="image" content="http://<?= $_SERVER['HTTP_HOST'] ?>/public/images/inspire-bg.jpg" />
